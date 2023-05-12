da1d2511ee054f020ef97cc579f980211ddfb676
brecert <me@bree.dev>
2023-05-12 11:49:32 -0400

    format main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const $ = (query) => document.querySelector(query)
 
 const withInput = (id, cb) => {
   const el = document.getElementById(id)
-  if(el) {
+  if (el) {
     el.oninput = (e) => cb(el)
     cb(el)
   }
@@ -21,19 +21,19 @@ const $logs = $('#projectorLogs')
 class State {
   #running = true
   #runtimeError = null
-  
+
   projector = new Projector($display)
 
   set running(value) {
     let startRunning = this.#running === false && value === true
     this.#running = value
-    if(startRunning) {
+    if (startRunning) {
       this.runCycle()
     }
   }
 
   set runtimeError(error) {
-    if(error != null) {
+    if (error != null) {
       $logs.value = error.toString()
     } else {
       $logs.value = ""
@@ -49,7 +49,7 @@ class State {
 
   handleError(cb) {
     try { cb() }
-    catch(err) {
+    catch (err) {
       this.runtimeError = err
     }
   }
@@ -58,20 +58,20 @@ class State {
     const ast = parse($input.value)
     const bytecode = BytecodeCompiler.compile(ast)
     this.projector.bytecode = bytecode
-    
+
     this.runtimeError = null
   }
 
   runCycle() {
     // console.log('cycle', this.#running, this.#runtimeError)
-    if(this.#running && this.#runtimeError == null) {
+    if (this.#running && this.#runtimeError == null) {
       this.projector.ctx.clearRect(0, 0, this.projector.canvas.width, this.projector.canvas.height)
-      
+
       this.handleError(() => {
         this.projector.render();
       })
     }
-    if(this.#running) requestAnimationFrame(() => this.runCycle())
+    if (this.#running) requestAnimationFrame(() => this.runCycle())
   }
 }
 
@@ -83,4 +83,6 @@ Object.keys(state.projector.options)
 withInput('run', (el) => state.running = el.checked)
 
 // do not do this
-withInput('laserCoordinates', (el) => state.projector.options.laserCoordinates = el.selectedIndex)
+withInput('laserCoordinates', (el) => {
+  state.projector.options.laserCoordinates = el.selectedIndex
+})
